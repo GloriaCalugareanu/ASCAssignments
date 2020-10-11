9d3c8cad51a6e884252a4b013860c75c4898e9bf
calugareanu.gloria@gmail.com <calugareanu.gloria@gmail.com>
2020-10-11 15:28:19 -0700

    Rezolvarea problemei "Legea lui Moore"

---
+++ Legea lui Moore/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Legea_lui_Moore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Consol;
+            double cateori;
+            while (true)
+            {
+                //Legea lui Moore presupune faptul ca puterea de calcul se dubleaza la fiecare 18 luni, iar pretul ramane acelasi
+
+                Console.WriteLine("Introduceti un numar n");
+                Consol = Console.ReadLine();
+                if (double.TryParse(Consol, out cateori))
+                {
+                    double ani;
+                    ani = 1.5 * Math.Log(cateori, 2);
+                    Console.WriteLine("In {1} ani, puterea de calcul se va mari de {0} ori", cateori, ani);
+
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Introduceti un alt numar");
+                    Console.ReadKey();
+                }
+
+
+            }
+        }
+    }
+}
